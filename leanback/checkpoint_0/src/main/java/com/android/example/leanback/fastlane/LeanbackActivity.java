5ea14513d24f0e8d392374b1e09895c30c5a2d33
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-08-05 12:10:46 +0900

    LeanbackActivityを追加

---
+++ leanback/checkpoint_0/src/main/java/com/android/example/leanback/fastlane/LeanbackActivity.java
@@ -0,0 +1,17 @@
+package com.android.example.leanback.fastlane;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.example.leanback.R;
+
+public class LeanbackActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leanback);
+    }
+
+
+}
