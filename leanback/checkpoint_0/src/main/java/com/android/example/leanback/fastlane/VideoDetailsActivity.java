c110e77217210a84ce82d564ca5c3152acf79ecb
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-08-16 14:38:34 +0900

    詳細Activityを追加

---
+++ leanback/checkpoint_0/src/main/java/com/android/example/leanback/fastlane/VideoDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.android.example.leanback.fastlane;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.example.leanback.R;
+
+public class VideoDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leanback_details);
+    }
+
+}
